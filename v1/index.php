2f6b70d46ccf25ec690e2ad37eeccccc17b4fe2f
ERKAN <bedirhanerkan@live.com>
2016-07-04 22:20:30 +0300

    Add files via upload

--- v1/index.php
+++ v1/index.php
@@ -1,1 +1,30 @@
+<?php
+/*
+ * Created by Bedirhan ERKAN
+ * Github   :  BedirhanERKAN
+ * Facebook :  BedirhanERKAN
+ * Twitter  :  BedirhanERKAN
+ * Youtube  :  BedirhanERKAN
+ */
+    require_once("router.php");
+    
+    $routers = new Router();
 
+    $routers::config(array("error_none" => 1));
+
+
+
+
+    Router::any("/","views/welcome.php",array());
+    Router::any("/index.html","views/welcome.php",array());
+
+    Router::any("/breaking-news/([0-9]+).html","views/breaking-news.php",array("news-id"));
+    Router::any("/breaking-news/(.*)-([0-9]+).html","views/breaking-news.php",array("news-title","news-id"));
+    Router::any("/webservice/v([0-9]+)/(.*)/(.*)/([0-9]+)","views/webservice.php",array("webservice-version","webservice-class","webservice-function","webservice-arg"));
+
+    Router::post("/kayit-et/([0-9]+).html","views/post.php",array("post_id"));
+
+    $routers->router();
+
+
+?>
---
+++ v1/router.php
@@ -0,0 +1,125 @@
+<?php
+/*
+ * Created by Bedirhan ERKAN
+ * Github   :  BedirhanERKAN
+ * Facebook :  BedirhanERKAN
+ * Twitter  :  BedirhanERKAN
+ * Youtube  :  BedirhanERKAN
+ */
+
+class Router{
+    
+    static $requestURI,$requestURIs,$scriptName,$requestlist = array(),$configlist = array();
+
+    function __construct()
+    {
+
+        self::$requestURIs = explode('/', $_SERVER['REQUEST_URI']);
+        self::$scriptName = explode('/',$_SERVER['SCRIPT_NAME']);
+       
+        for($i= 0;$i < sizeof(self::$scriptName);$i++)
+        {
+            if (self::$requestURIs[$i] == self::$scriptName[$i])
+            {
+                unset(self::$requestURIs[$i]);
+            }
+        }
+        
+        $newurl = null;
+        foreach(self::$requestURIs as $val){
+            $newurl.= "/".$val;
+        }
+        self::$requestURI = $newurl;
+        
+    }
+
+    function any($href,$callback,$arr = array())
+    {
+        self::$requestlist[] = array($href,$callback,$arr,0);
+    }
+    
+    
+    function post($href,$callback,$arr = array())
+    {
+        self::$requestlist[] = array($href,$callback,$arr,1);
+    }
+
+    function config($e)
+    {
+        self::$configlist = $e;
+    }
+
+    function router()
+    {
+        global $pars;
+        $errorcode = 0;
+        
+        for($i=0; $i < sizeof(self::$requestlist);$i++)
+        {        
+            //preg_match_all("@". self::$requestlist[$i][0] ."@i", $_SERVER['REQUEST_URI'], $pat_array);
+            preg_match_all("@". self::$requestlist[$i][0] ."@i", self::$requestURI, $pat_array);
+
+            if(self::$requestlist[$i][3]==1 and $_SERVER['REQUEST_METHOD']!="POST"){ break; }
+
+            //echo $pat_array[0][0]."<>".self::$requestURI."<br>";
+            if(@$pat_array[0][0]==self::$requestURI)
+            {
+                unset($pat_array[0]);
+                $params = $pat_array;
+
+
+                $is=0;
+                foreach($params as $key => $val)
+                {
+                     self::$requestlist[$i][1] = str_ireplace(":".self::$requestlist[$i][2][$is],$val[0],self::$requestlist[$i][1]);
+
+                     $pars[self::$requestlist[$i][2][$is]]=$val[0];
+                     $_GET[self::$requestlist[$i][2][$is]] = $val[0];
+                     $is++;
+                }
+
+                if($_SERVER['REQUEST_METHOD']=="POST")
+                {
+                    foreach($_POST as $valuesm => $item){
+                        $pars[$valuesm] = $item;
+                    }
+                }
+
+                $path = self::$requestlist[$i][1];
+
+                if(file_exists($path))
+                {
+                 require_once($path);   
+                }else{
+                    header("HTTP/1.0 404 Not Found");
+
+                    if(self::$configlist['error_none']==false)
+                    {
+                     echo self::$requestlist[$i][1]." not found.";
+                    }else{
+                        echo "404 Not Found";
+                    }
+
+                    die();
+                }  
+                
+                
+                break;
+            }else{
+                    $errorcode+=1;
+
+                    if(sizeof(self::$requestlist)==$errorcode)
+                    {
+                       header("HTTP/1.0 404 Not Found");
+                        echo "Check your router list!";
+                       die();
+
+                    }
+            }
+             
+        }
+        
+        return $pars;
+    }
+
+}
